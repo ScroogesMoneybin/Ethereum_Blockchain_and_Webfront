1f463eb689afc49c2e5443ca3f055374496cbad3
ScroogesMoneybin <107962165+ScroogesMoneybin@users.noreply.github.com>
2022-11-23 13:10:48 -0500

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from "./web3"; //import instance of web3 from ./web file
 import CampaignFactory from "./compiled/CampaignFactory.json";
 const instance= new web3.eth.Contract(
     CampaignFactory.abi,
-    "0x10e912c6792B2a57a8c1a16ddf4Ff777b088bF24"
+    "0xBcBB894E9278f87a3b68cC9b1D9435405973c57A"
 );
 
 export default instance;
